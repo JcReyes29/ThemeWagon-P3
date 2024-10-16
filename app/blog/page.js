a0916fa08cc53991df9fb37be49bf437ef8b83cd
JcReyes29 <j.creyesp29@gmail.com>
2024-10-16 04:56:59 -0500

    blog and contact pages

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,14 @@
-import React from 'react'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <>
+      <div className=''>
+      <header className='flex flex-col items-center py-3 gap-4 md:mx-96 px-28 sm:mx-auto'>
+          <h1 className='text-3xl font-semibold font-sans'>Blog post</h1>
+          <p className='dark:text-gray-500 text-center'>Blogs are a great method to increase traffic and generate leads. Make a profit. Get compensated for your efforts.</p>
+        </header>
+      </div>
+    
+    </>
   )
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,34 @@
-import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <div className=''>
+        <header className='flex flex-col items-center py-3 gap-4 md:mx-96 px-28 sm:mx-auto'>
+          <h1 className='text-3xl font-semibold font-sans'>Contact Us</h1>
+        </header>
+        <section className='flex w-full justify-center my-20'>
+
+          <div className="w-full max-w-sm p-4 bg-transparent border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
+            <form className="space-y-6" action="#">
+              <div>
+                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:text-white" placeholder="Write your name" required />
+              </div>
+              <div>
+                <input type="password" name="password" id="password" placeholder="Write your email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:text-white" required />
+              </div>
+              <div className="flex items-start">
+                <div className="flex">
+                  <textarea id="description" rows="4" className=" p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write event description..."></textarea>
+                </div>
+              </div>
+              <button type="submit" className="bg-violet-300 w-full text-white focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center ">Submit</button>
+            </form>
+          </div>
+
+
+        </section>
+      </div>
+
+    </>
   )
 }
